e6f32f7943396ec4b5ea1fe7f900bdf069e395a9
Pa Khan <don@pa-khan.com>
2025-01-06 01:13:04 +0300

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/budka-tech/iport
+
+go 1.23.3
+
+require github.com/budka-tech/envo v0.0.2
---
+++ iport.go
@@ -0,0 +1,51 @@
+package iport
+
+import (
+	"fmt"
+	"github.com/budka-tech/envo"
+)
+
+type Port uint16
+type Host string
+
+type Ports struct {
+	env *envo.Env
+	m   map[Host]Port
+}
+
+type Params struct {
+	env *envo.Env
+	m   map[Host]Port
+}
+
+func NewPorts(params Params) *Ports {
+	return &Ports{
+		env: params.env,
+		m:   params.m,
+	}
+}
+
+func (p *Ports) FormatServiceTCP(name Host) string {
+	return FormatServiceTCP(p.env, name, p.m)
+}
+
+func Format(port int16) string {
+	return fmt.Sprintf("%d", port)
+}
+
+func FormatTCP(port int16) string {
+	return fmt.Sprintf(":%d", port)
+}
+
+func FormatServiceTCP(env *envo.Env, name Host, m map[Host]Port) string {
+	port := m[name]
+	if env.IsLocal() {
+		return FormatLocal(port)
+	} else {
+		return fmt.Sprintf("%s:%d", name, port)
+	}
+}
+
+func FormatLocal(port Port) string {
+	return fmt.Sprintf("localhost:%d", port)
+}
